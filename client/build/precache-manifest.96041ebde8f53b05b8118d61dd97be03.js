a3d354e07ad3976d573441b8f969ed7077090b05
hanshank <hans@hanken.org>
2018-12-20 01:47:02 -0800

    testings

---
+++ client/build/precache-manifest.96041ebde8f53b05b8118d61dd97be03.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c35416579a163c1c09a",
+    "url": "/static/css/main.d6d18396.chunk.css"
+  },
+  {
+    "revision": "2c35416579a163c1c09a",
+    "url": "/static/js/main.2c354165.chunk.js"
+  },
+  {
+    "revision": "fb8eb1749ca9039653f8",
+    "url": "/static/js/1.fb8eb174.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a6221ab9d36b96624f3787601243845b",
+    "url": "/static/media/logo.a6221ab9.svg"
+  },
+  {
+    "revision": "5d7f6541e3d08d7713f69d5ae9f28ac8",
+    "url": "/static/media/pedestrians.5d7f6541.mp4"
+  },
+  {
+    "revision": "0aeb02b63f25f4fb914e22e7cf93cbcb",
+    "url": "/static/media/laptop.0aeb02b6.png"
+  },
+  {
+    "revision": "9f68b850b27c36f8bddd17d94d1d2dfe",
+    "url": "/index.html"
+  }
+];
