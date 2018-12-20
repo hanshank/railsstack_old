66474b1b2ed8c2f168644bce7df56ad9f643d217
hanshank <hans@hanken.org>
2018-12-19 23:56:04 -0800

    fixed

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,5 @@ Rails.application.routes.draw do
     end
   end
 
-  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
-    !request.xhr? && request.format.html?
-  end
+  get '/*path', to: 'application#fallback_index_html'
 end
