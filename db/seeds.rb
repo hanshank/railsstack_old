cce614c174a01fb43da7a6854c7a66961250041b
hanshank <hans@hanken.org>
2018-12-24 10:30:43 -0800

    new seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@ AdminUser.create!(email: 'admin@example.com', password: 'password', password_con
 
 User.create(name: "Hans Martin Hanken", email: "hansmhank@gmail.com", id: 1)
 
-Post.create(title: 'My vey first post', introduction: "A very short introduction that is on point. It will catch the readers attention.", content: 'Lorem ipsos is the stuff', user_id: 1)
+Post.create(title: 'My very first post', introduction: "A very short introduction that is on point. It will catch the readers attention.", content: 'Lorem ipsos is the stuff', user_id: 1)
+Post.create(title: 'My very second post', introduction: "A very short introduction that is on point. It will catch the readers attention.", content: 'Lorem ipsos is the stuff', user_id: 1)
 
 
