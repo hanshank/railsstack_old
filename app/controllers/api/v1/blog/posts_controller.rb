81f822adda1536c7ff7e121390e12fa978270f97
hanshank <hans@hanken.org>
2018-12-26 10:37:04 -0800

    fix for api call

--- app/controllers/api/v1/blog/posts_controller.rb
+++ app/controllers/api/v1/blog/posts_controller.rb
@@ -15,12 +15,12 @@ module Api::V1::Blog
 
     def show
       @post.published_at = @post.published_at.strftime("%h")
-      render json: @post, include: :user, include: [{image: {include: {attachments: {include: {blob: {methods: :image_url}}}}}}]
+      render json: @post, include: :user
     end
 
     def index
       @posts = Post.all.where.not(published_at: nil).order(published_at: :desc)
-      render json: @posts, include: [{image: {include: {attachments: {include: {blob: {methods: :image_url}}}}}}]
+      render json: @posts, include: :image, methods: :image_url
     end
 
     private
