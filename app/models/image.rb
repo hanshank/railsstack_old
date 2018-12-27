2d74ed85e6ea7caafec4acfd2273be94f8943af9
hanshank <hans@hanken.org>
2018-12-26 21:28:53 -0800

    some change

--- app/models/image.rb
+++ app/models/image.rb
@@ -5,11 +5,11 @@ class Image < ApplicationRecord
     belongs_to :imageable, polymorphic: true, optional: true
     has_one_attached :file
 
-    before_save :set_image_url
+    before_save :set_file_url
 
-    validates :alt_attribute, :url, presence: true
+    validates :alt_attribute, presence: true
 
-    def set_image_url
+    def set_file_url
         self.url = rails_blob_path(self.file) if self.file.attached?
     end
 end
